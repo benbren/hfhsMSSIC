935785f6dc4184becb5282817ab802bc2cc7b3bd
Ben Brennan <brennben@umich.edu>
2024-08-06 09:57:00 -0400

    different site IDS to remove

--- R/remove_sites.R
+++ R/remove_sites.R
@@ -12,11 +12,11 @@ remove.sites = function(dat,additional.sites = NULL) {
     install.packages("dplyr")
     library(dplyr)
     }
-  remove = c(519, #  Covenant Medical Center
-             523, # Ascension Genesys Hospital
-             524, # Beaumont - Grosse Pointe
-             521, # DMC Harper University Hospital
-             525, # DMC Detroit
+  remove = c(519, 6819, #  Covenant Medical Center
+             523, 6825, # Ascension Genesys Hospital
+             524, 6813, # Beaumont - Grosse Pointe
+             521, 20227, # DMC Harper University Hospital
+             525, 21790, # DMC Detroit
              additional.sites)
   dat <- dat |> filter(!(Siteid %in% remove))
   dat
